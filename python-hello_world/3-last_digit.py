27c6dc1e3e349e0325f3669ff8f2365ec3b187c6
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-11-30 12:47:18 +0200

    commit 11

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,4 +1,4 @@
 import random
 number = random.randint(-10000, 10000)
-last=number
+last = (number[1])
 print("Last digit of",number,"is",last)
---
+++ python-hello_world/4-print_hexa.py
@@ -0,0 +1,11 @@
+for i in range (99):
+
+
+      while i<= 98 :
+         if i<10:
+            print (i,"=","0*",i)
+         elif i>=10:  
+             print (i,"=","a")
+      i=i+1       
+          
+
