d7837f32246ee2d38dc8c40dc2272a15c2a007c2
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-11-30 20:35:35 +0200

    commit 15

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,11 +1,7 @@
 for i in range (99):
-
-
-      while i<= 98 :
-         if i<10:
-            print (i,"=","0*",i)
-         elif i>=10:  
-             print (i,"=","a")
-      i=i+1       
+   for j in range ():
+     
+       print (i,"="+"0"+"*"+str(j))
+           
           
 
