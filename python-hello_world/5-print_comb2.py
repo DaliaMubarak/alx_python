a6559f22ceaf0506bf666fe9fa5d2cee35f26b2f
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-01 00:02:38 +0200

    commit 18

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 for i in range(100):
      if i<99:
        print (str("{}").format(i).zfill(2),",",end="")
-     else:print (str("{}").format(i).zfill(2),end="")
+     else:print (str("{} ").format(i).zfill(2),end="")
      
          
          
