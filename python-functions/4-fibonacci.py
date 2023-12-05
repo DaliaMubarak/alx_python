9af7419bd222c16899c20af6df990eab581f60bf
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-05 13:55:18 +0200

    commit 41

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,1 +1,13 @@
-def fibonacci_sequence(n)
+def fibonacci_sequence(n):
+ a=0
+ b=1
+ 
+ if n <1:
+   return list()
+ elif n==1:
+    return a
+ for i in range (2,n):
+       c=a+b
+       a=b
+       b=c
+       return list()
