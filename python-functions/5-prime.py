c8dceed496cbe3d47016b7458796de8a47d56877
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-05 12:00:25 +0200

    comit 38

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,9 @@
 def is_prime(number):
-    for i in range (2,number):
-        if number % i == 0:
-
-          return True
+    if number == 0:
+        return False
     else:
-          return False
+         for i in range (2,number):
+            if number % i == 0:
+             return False
+            else:
+            return True
