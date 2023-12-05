fc4a709fb5ad7d2a677dad0aa4054f2faa0896e7
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-05 10:50:41 +0200

    commit 32

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -7,11 +7,7 @@ def validate_password(password):
         for character in password:
              if character.isspace():
                   return False
-             elif character.isupper():
-                  return True
-             if character.islower():
-                  return True
-             if character.isdigit():
+             elif any(character.isupper()) and any (character.islower()) and any(character.isdigit()):
                   return True
              else:
                  return False
