5f19b7879affb94da22099c60eb618f50f812ae1
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-05 11:22:24 +0200

    commit 36

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -1,18 +1,15 @@
 def validate_password(password):
       if len(password) <8:
             return False
-      elif ' ' in password:
-                  return False
+      
       
       else:
             for character in password:
              
-             if character.isupper():
-                  return True
-             if character.islower():
-                  return True 
-             if character.isdigit():
-                  return True
+             if not (character.isupper() and character.islower() and character.isdigit()):
+                  return False
+             elif ' ' in password:
+                  return False
              else:
                   return False
            
