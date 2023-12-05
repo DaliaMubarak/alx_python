f26a37e04be5eb8c7301512f884b8b170502c35c
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-05 12:25:47 +0200

    commit 40

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,10 @@
 def is_prime(number):
     if number <=0:
-        return False
+        print (False )
     else:
         for i in range (2,number):
             if number % i == 0:
-             print False
+             print (False )
              break
         else:
-             print True
+             print (True )
