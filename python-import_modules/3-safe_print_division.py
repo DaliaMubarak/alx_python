965f858c921b6601c5cd1bbcb03195ddf279e04f
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-07 12:16:39 +0200

    47

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,23 +1,14 @@
 
 
 def safe_print_division(a, b):
- if b==0:
-   return None
- else:
-   return a/b
  
-
-
-try:
+ try:
    a/b
-
-except:
+ except:
+  return "none"
+ else:
+   return (a/b)
+ finally:
   print ("inside result: {}".format(safe_print_division(a,b))) 
  
-else:
-  print("inside result: {}".format(safe_print_division(a,b)))
-
-finally:
-   print ("{} / {} = {} ".format(a,b,(safe_print_division(a,b))))
-
-safe_print_division(a,b)
+ 
