4a1c6f182f086d7dd072348fb6f943fa785aac7c
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-08 01:12:02 +0200

    60

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,3 +1,16 @@
-a=len(argv)
 
-def 
+
+
+for arg in argv:
+  a=len(argv)
+  if a==0:
+    print ("0 arguments.")
+  elif a==1:
+    print ("1 argument:")
+    print("1:  {}".format(arg))
+  else:
+    print("{}: {}".format(argv.index(arg),arg))
+
+  
+      
+
