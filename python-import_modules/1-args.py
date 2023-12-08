bf6dc6cf47a2e45968f237f79f8f8186a2a6abc7
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-09 01:19:17 +0200

    72

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -6,11 +6,11 @@ if a==0:
     print ("0 arguments.")
 elif a==1:
     print ("1 argument:")
-    print("1:{}".format(sys.argv[1]))
+    print("1: {}".format(sys.argv[1]))
 else:
     print ("{} arguments:".format(a))
     for i in range (a):
-      print("{}: {}".format(i,sys.argv[i]))
+      print("{}: {}".format(i+1,sys.argv[i+1]))
 
   
       
