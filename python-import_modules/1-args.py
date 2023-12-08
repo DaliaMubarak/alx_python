112ba7708c9a63f23825139e1f8d672f2923db9f
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-09 01:11:06 +0200

    69

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,15 @@
 if __name__=="__main__":
    import sys
-for arg in sys.argv:
-  a=len(sys.argv)-1
-  if a==0:
+
+a=len(sys.argv)-1
+if a==0:
     print ("0 arguments.")
-  elif a==1:
+elif a==1:
     print ("1 argument:")
-    print("1: {}".format(arg))
-  else:
+else:
     print ("{} arguments:".format(a))
-    break
-for arg in sys.argv[1:]:
-    if  a>1:
-      print("{}: {}".format(sys.argv.index(arg),arg))
+for i in range (a):
+      print("{}: {}".format(i,sys.argv[i]))
 
   
       
