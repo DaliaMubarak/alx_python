7ae3e7f71aa529588d5bf4da8792cd42303c9ba9
Dlia Mubarak <dalia.a.hameed@gmail.com>
2023-12-12 14:24:13 +0200

    93

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,6 @@
 def no_c(my_string):
-    
-    for char in my_string:
-        if char=="c":
-         return my_string.translate({ord('c'): None})
-        elif char=="C":
-          return my_string.translate({ord('C'): None}) 
+    lower= my_string.lower()
+    return lower.translate({ord("c"): ""})
+        
+        
+print(no_c())
